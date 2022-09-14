5b5ae9ea74e15bfb6917e6dff22cffbf146045f3
Appy26 <112808903+Appy26@users.noreply.github.com>
2022-09-14 16:45:50 +0530

    Add files via upload

---
+++ Day-1-Problems/problem2.js.js
@@ -0,0 +1,15 @@
+//Define a variable called name with your Name as the assigned value
+//Print the value stored in the variable name
+//Change the variable to store your father's name
+//Print the value stored in the variable name
+//Change the variable again to store your mother's name.
+//Print the value stored in the variable name
+
+let name="Aparna Singh"
+console.log("My Name: "+name)
+
+name1= "Rajiv Singh"
+console.log("Father's Name: "+name1)
+
+name2="Punita Singh"
+console.log("Mother's Name: "+name2)
---
+++ Day-1-Problems/problem4.js.js
@@ -0,0 +1,31 @@
+let name ="NAME = Harsh chouhan";
+
+console.log(name);
+
+let school="SCHOOL= saraswati vidya mandir";
+
+console.log(school);
+
+let grade ="GRADE = 9";
+
+console.log(grade);
+
+let section ="SECTION = B";
+
+console.log(section);
+
+let rollnumber ="ROLL NUMBER= 2";
+
+console.log(rollnumber);
+
+let marksinmaths ="MARKS IN MATHS = 92";
+
+console.log(marksinmaths);
+
+let marksincomputer ="MARKS IN COMPUTER =97";
+
+console.log(marksincomputer);
+
+let marksinscience = "MARKS IN SCIENCE = 89";
+
+console.log(marksinscience);
